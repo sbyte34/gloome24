59d556ae3e1d73a4049207240895d6d3b45e4ae2
Faizan <20010940@ycce.in>
2023-12-29 01:31:12 +0530

    Netlify

--- src/pages/FinanceManagement/Earning.jsx
+++ src/pages/FinanceManagement/Earning.jsx
@@ -1,20 +1,9 @@
 import React, { useState } from 'react';
 import Sidebar from '../../partials/Sidebar';
-import WelcomeBanner from '../../partials/dashboard/WelcomeBanner';
-import DashboardCard01 from '../../partials/dashboard/DashboardCard01';
-import DashboardCard02 from '../../partials/dashboard/DashboardCard02';
 import DashboardCard03 from '../../partials/dashboard/DashboardCard03';
-import DashboardCard12 from '../../partials/dashboard/DashboardCard12';
-import DashboardAvatars from '../../partials/dashboard/DashboardAvatars';
-import DashboardCard04 from '../../partials/dashboard/DashboardCard04';
-import DashboardCard05 from '../../partials/dashboard/DashboardCard05';
-import DashboardCard06 from '../../partials/dashboard/DashboardCard06';
-import DashboardCard07 from '../../partials/dashboard/DashboardCard07';
 import DashboardCard08 from '../../partials/dashboard/DashboardCard08';
 import DashboardCard09 from '../../partials/dashboard/DashboardCard09';
-import DashboardCard10 from '../../partials/dashboard/DashboardCard10';
 import DashboardCard11 from '../../partials/dashboard/DashboardCard11';
-import DashboardCard13 from '../../partials/dashboard/DashboardCard13';
 import Header from '../../partials/Header';
 
 const Earning = () => {
